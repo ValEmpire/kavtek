8cff351f57ad9200075a9576931db76751feabc0
ValEmpire <palma_arcival@yahoo.com>
2022-05-11 16:28:39 -0400

    incorp theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { Provider } from "react-redux";
 import { createStore } from "./redux/store";
 import "./App.css";
+import IndexPage from "./pages";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
 
 function App() {
-  return <Provider store={createStore()}></Provider>;
+  return (
+    <Provider store={createStore()}>
+      <ThemeProvider theme={theme}>
+        <IndexPage />
+      </ThemeProvider>
+    </Provider>
+  );
 }
 
 export default App;
